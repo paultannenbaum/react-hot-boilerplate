b15960ea200a20475ee8b4431174f7da0051a018
Paul Tannenbaum <paultannenbaum@gmail.com>
2015-02-05 18:32:35 -0800

    Tile population and logic

--- scripts/app.js
+++ scripts/app.js
@@ -7,24 +7,47 @@ var App = React.createClass({
   propTypes: {},
   mixins:    [],
 
-  getInitialState: function() { return {}; },
+  getInitialState: function() {
+    return {
+      player: 'X'
+    };
+  },
   getDefaultProps: function() {},
   componentWillMount: function() {},
   componentWillReceiveProps: function() {},
   componentWillUnmount: function() {},
 
+  handleTileClick: function(tile) {
+    if (!tile.state.value) {
+      this.setTileState(tile);
+      this.switchPlayers();
+    }
+  },
+
+  setTileState: function(tile) {
+    return tile.setState({
+      value: this.state.player
+    });
+  },
+
+  switchPlayers: function() {
+    return this.setState({
+      player: this.state.player === 'X' ? 'O' : 'X'
+    });
+  },
+
   render: function() {
     return (
       <div id='board'>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
+        <Tile onClick={this.handleTileClick} />
       </div>
     );
   }
--- scripts/tile.js
+++ scripts/tile.js
@@ -3,18 +3,30 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  propTypes: {},
+  propTypes: {
+    onClick: React.PropTypes.func
+  },
   mixins:    [],
 
-  getInitialState: function() { return {}; },
+  getInitialState: function() {
+    return {
+      value: ''
+    };
+  },
   getDefaultProps: function() {},
   componentWillMount: function() {},
   componentWillReceiveProps: function() {},
   componentWillUnmount: function() {},
 
+  registerCallback: function() {
+    this.props.onClick(this);
+  },
+
   render: function() {
     return (
-      <div className='tile'></div>
+      <div className='tile' onClick={this.registerCallback}>
+        {this.state.value}
+      </div>
     );
   }
 });
