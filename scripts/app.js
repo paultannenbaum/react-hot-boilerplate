5bd6afea960ee819f65567ef989e8962c75931db
Paul Tannenbaum <paultannenbaum@gmail.com>
2015-02-13 15:24:40 -0800

    promisified app.js to account for async in setState

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React   = require('react');
+var Q       = require('q');
 var _       = require('underscore');
 var Tile    = require('./tile');
 var Header  = require('./header');
@@ -47,23 +48,25 @@ var App = React.createClass({
 
   handleTileClick: function(tile) {
     if (this.state.gameStatus === 'open' && !tile.state.value) {
-      this.setTileState(tile);
-      this.recordTilePlay(tile);
-      this.analyzeBoardForWinOrDraw();
-
-      if (this.state.gameStatus === 'open') {
-        this.switchPlayers();
-      }
+      this.setTileState(tile)
+      .then(this.recordTilePlay)
+      .then(this.analyzeBoardForWinOrDraw)
+      .then(this.switchPlayers);
     }
   },
 
   setTileState: function(tile) {
+    var d = Q.defer();
+
     tile.setState({
       value: this.state.player
-    });
+    }, function() { d.resolve(tile) });
+
+    return d.promise;
   },
 
   recordTilePlay: function(tile) {
+    var                 d = Q.defer();
     var boardArrayRef   = this.state.boardValues;
     var historyArrayRef = this.state.playHistory;
     var row             = tile.props.position[0];
@@ -79,17 +82,21 @@ var App = React.createClass({
     this.setState({
       boardValues: boardArrayRef,
       playHistory: historyArrayRef
-    });
+    }, d.resolve);
+
+    return d.promise;
   },
 
   // This is whole method is pretty shitty, but its just a prototype
   analyzeBoardForWinOrDraw: function() {
+    var d = Q.defer();
+
     if (!_.contains(_.flatten(this.state.boardValues), null)) {
       this.setState({
         gameStatus: 'draw'
-      });
+      }, d.reject);
     } else {
-      _.each(this.props.possibleWinRoutes, function(winRoute) {
+      _.each(this.props.possibleWinRoutes, function(winRoute, index) {
         var routeState = '';
 
         _.each(winRoute, function(cellPosition) {
@@ -102,10 +109,14 @@ var App = React.createClass({
         if (routeState === 'XXX' || routeState === 'OOO') {
           this.setState({
             gameStatus: 'closed'
-          });
+          }, d.reject);
+        } else if (index == this.props.possibleWinRoutes.length-1) {
+          d.resolve()
         }
       }, this);
     }
+
+    return d.promise;
   },
 
   switchPlayers: function() {
